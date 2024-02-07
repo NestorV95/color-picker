f27b66ba3a47aaedd3b7e9867a945e6f701ca8ac
Nestor Venegas <nestor.venegas95@gmail.com>
2024-02-06 19:33:59 -0600

    Update useImage.tsx

--- src/hooks/useImage.tsx
+++ src/hooks/useImage.tsx
@@ -3,13 +3,13 @@ import { useState } from 'react';
   Image by Pawel Czerwinski 
   https://unsplash.com/photos/pink-and-blue-abstract-painting-sxaYEsE12RM?utm_content=creditShareLink&utm_medium=referral&utm_source=unsplash
 */
-import exampleImage from '@/assets/pawel-czerwinski-sxaYEsE12RM-unsplash.jpg';
+import exampleImage from '@/assets/images/pawel-czerwinski-sxaYEsE12RM-unsplash.jpg';
 import { ACCEPTED_MIME_TYPES, DEFAULT_IMAGE_STATE } from '@/config/constants';
 
 export default function useImage() {
 	const [state, setState] = useState(DEFAULT_IMAGE_STATE);
 
-	const resetImage = () => () => setState(DEFAULT_IMAGE_STATE);
+	const resetImage = () => setState(DEFAULT_IMAGE_STATE);
 
 	const setUploadedImage = (e: React.SyntheticEvent) => {
 		const fileInput = e.target as HTMLInputElement;
